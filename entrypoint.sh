50d5d269f21e556f0c722a5abe2aa4e4c0db32a5
Feng Yu <yu.feng@shifudao.com>
2016-09-13 21:40:20 +0800

    delete cache and tmp for upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
 [ -d "${NEXUS_DATA}" ] || mkdir -p "${NEXUS_DATA}"
-chown -R nexus:nexus "${NEXUS_DATA}"
+[ $(stat -c '%U' "${NEXUS_DATA}") != 'neuxs' ] && chown -R nexus:nexus "${NEXUS_DATA}"
+
+# clear tmp and cache for upgrade
+rm -fr "${NEXUS_DATA}"/tmp/ "${NEXUS_DATA}"/cache/
 
 [ -n "${JAVA_MAX_MEM}" ] && sed -i "s/-Xmx.*/-Xmx${JAVA_MAX_MEM}/g" /opt/sonatype/nexus/bin/nexus.vmoptions
 [ -n "${JAVA_MIN_MEM}" ] && sed -i "s/-Xms.*/-Xmx${JAVA_MIN_MEM}/g" /opt/sonatype/nexus/bin/nexus.vmoptions
